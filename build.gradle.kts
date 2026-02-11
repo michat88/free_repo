6e47270d58fa27955aedd14dd6d054f526457e3a
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-11 11:50:49 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,18 +8,13 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // JitPack wajib ada di sini
         maven("https://jitpack.io")
     }
 
     dependencies {
-        // Plugin Android Stabil
+        // Kita kunci ke versi stabil, jangan pakai SNAPSHOT
         classpath("com.android.tools.build:gradle:8.2.2")
-        
-        // KITA BALIK KE MASTER-SNAPSHOT (Sekarang aman karena Gradle udah v8.6)
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        
-        // Kotlin Stabil
+        classpath("com.github.recloudstream:gradle:v0.1.4") // <-- GANTI KE INI
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
@@ -42,7 +37,6 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // Ganti URL ini dengan URL repo GitHub kamu sendiri kalau mau
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/cloudstream-extensions-phisher")
         authors = listOf("Phisher98")
     }
@@ -52,7 +46,7 @@ subprojects {
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(34) // Kita pake 34 aja biar stabil
+            compileSdkVersion(34)
             targetSdk = 34
         }
 
@@ -77,19 +71,14 @@ subprojects {
         val implementation by configurations
         val cloudstream by configurations
         
-        // CloudStream Core Stabil
         cloudstream("com.lagradost:cloudstream3:pre-release") 
 
-        // Standard libs
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.17.2")
-        
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
         implementation("com.google.code.gson:gson:2.10.1")
-        
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-        
         implementation("org.mozilla:rhino:1.7.14") 
         implementation("me.xdrop:fuzzywuzzy:1.4.0")
     }
