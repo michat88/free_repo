7655d60b09c6e7a09006fa881e45866295c72925
michat88 <gokilmichat@gmail.com>
2026-09-16 15:41:25 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,15 +27,23 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(
+    configuration: CloudstreamExtension.() -> Unit
+) = extensions
+    .getByName<CloudstreamExtension>("cloudstream")
+    .configuration()
 
-fun Project.android(configuration: LibraryExtension.() -> Unit) {
+fun Project.android(
+    configuration: LibraryExtension.() -> Unit
+) {
     extensions.getByName<LibraryExtension>("android").apply {
-        project.extensions.findByType(JavaPluginExtension::class.java)?.apply {
-            toolchain {
-                languageVersion.set(JavaLanguageVersion.of(17))
+        project.extensions
+            .findByType(JavaPluginExtension::class.java)
+            ?.apply {
+                toolchain {
+                    languageVersion.set(JavaLanguageVersion.of(17))
+                }
             }
-        }
 
         configuration()
     }
@@ -46,12 +54,17 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/trinityzanetamanu-code/Premium_RepoX")
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "https://github.com/michat88/free_repo"
+        )
+
         authors = listOf("trinityzanetamanu")
     }
 
     android {
         namespace = "com.trinityzanetamanu"
+
         compileSdk = 36
 
         defaultConfig {
@@ -62,15 +75,26 @@ subprojects {
             targetSdk = 36
         }
 
+        /*
+         * CloudStream pre-release terbaru menggunakan JVM target 11.
+         *
+         * Karena itu Java dan Kotlin provider juga harus menghasilkan
+         * bytecode JVM 11 agar fungsi inline dari cloudstream.jar
+         * dapat dikompilasi tanpa error:
+         *
+         * Cannot inline bytecode built with JVM target 11 into
+         * bytecode that is being built with JVM target 1.8
+         */
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
         }
 
         //noinspection WrongGradleMethod
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
+                jvmTarget.set(JvmTarget.JVM_11)
+
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -83,25 +107,50 @@ subprojects {
     dependencies {
         val implementation by configurations
         val cloudstream by configurations
+
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
         // Fix for Kotlin 2.4.0 strict type annotation checking
         implementation("org.jspecify:jspecify:1.0.0")
 
-        // Other dependencies
+        // Kotlin
         implementation(kotlin("stdlib"))
+
+        // Networking
         implementation("com.github.Blatzar:NiceHttp:0.4.18")
+
+        // HTML parser
         implementation("org.jsoup:jsoup:1.22.2")
+
+        // Android annotations
         implementation("androidx.annotation:annotation:1.10.0")
+
         // Do not bump above 2.13.1
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+        implementation(
+            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
+        )
+        implementation(
+            "com.fasterxml.jackson.core:jackson-databind:2.13.1"
+        )
+
+        // Coroutines
+        implementation(
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
+        )
+
         // Do not bump above 1.8.1
         implementation("org.mozilla:rhino:1.8.1")
+
+        // Fuzzy matching
         implementation("me.xdrop:fuzzywuzzy:1.4.0")
+
+        // JSON
         implementation("com.google.code.gson:gson:2.14.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+        implementation(
+            "org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0"
+        )
+
+        // Crypto
         implementation("org.bouncycastle:bcpkix-jdk18on:1.84")
     }
 }
